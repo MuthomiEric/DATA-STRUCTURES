662a82224a4afcc200635ac0a95af0bc1a599f66
your_username <muthomi102@gmail.com>
2022-01-08 11:34:12 +0300

    array operations

---
+++ DATA STRUCTURES/Arrays/ArrayOperations.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATA_STRUCTURES.Arrays
+{
+    public class ArrayOperations
+    {
+        public void TraverseThroughJaggedArray(int[][] jaggedArray)
+        {
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                   
+                    Console.Write($"{jaggedArray[i][j]} ");
+                }
+
+                Console.WriteLine();
+
+            }
+
+        }
+
+        public char[] reverseArray(char[] arr)
+        {
+            int start = 0, end = arr.Length - 1;
+
+            while (start < end)
+            {
+                char temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+
+            return arr;
+        }
+    }
+}
--- DATA STRUCTURES/Program.cs
+++ DATA STRUCTURES/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DATA_STRUCTURES.Arrays;
+using System;
 
 namespace DATA_STRUCTURES
 {
@@ -6,7 +7,24 @@ namespace DATA_STRUCTURES
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ArrayOperations arrayOperations = new ArrayOperations();
+
+            var name = "Muthomi";
+            var b = name.ToCharArray();
+
+            var a = arrayOperations.reverseArray(b);
+
+            int[][] arrs = new int[4][];
+
+            arrs[0] = new int[] { 1, 2, 3, 4 };
+            arrs[1] = new int[] { 5, 6, 7 };
+            arrs[2] = new int[] { 8, 9 };
+            arrs[3] = new int[] { 10, 11, 12, 13, 14, 15 };
+
+            arrayOperations.TraverseThroughJaggedArray(arrs);
+
+
         }
+
     }
 }
